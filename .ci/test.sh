dc848ccba2b631ad95112b09c38ba17def284918
Carl Kittelberger <icedream@icedream.pw>
2021-10-28 07:36:48 +0200

    Add test script.

---
+++ .ci/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+errors=()
+
+while read -d $'\0' customizepkgfile
+do
+	package="$(basename "$customizepkgfile")"
+	echo "** Testing package: $package" >&2
+
+	srcdir=$(mktemp -p /var/tmp -d)
+	(
+		git clone --depth=1 "https://aur.archlinux.org/$package.git" "$srcdir"
+		export CUSTOMIZEPKG_CONFIG="$(pwd)"
+
+		cd "$srcdir"
+		customizepkg
+		echo "** Test for package $package PASSED" >&2
+	) || (
+		echo "** Test for package $package FAILED, recording for later" >&2
+		errors+=("Package $package did not pass")
+	)
+	rm -r "$srcdir"
+	echo ""
+done < <(find -maxdepth 1 -type f -print0)
+
+if [ "${#errors}" -gt 0 ]
+then
+	echo "Following ERRORs occurred:" >&2
+	for error in "${errors[@]}"
+	do
+		echo "  - $error" >&2
+	done
+else
+	echo "All tests passed, OK." >&2
+fi
